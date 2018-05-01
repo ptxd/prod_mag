c6c867cadc3f6d9b308c75f1efdc1cfa11dc3f43
thr33 <ptxdas@gmail.com>
2018-05-01 11:04:54 -0700

    fix social media logo

--- src/components/footer.js
+++ src/components/footer.js
@@ -16,8 +16,8 @@ const Footer = () => {
                     <div className="socials">
                         <a href="#" className="social social-facebook" aria-label="facebook"><i className="ui-facebook"></i></a>
                         <a href="#" className="social social-twitter" aria-label="twitter"><i className="ui-twitter"></i></a>
-                        <a className="social " href="#" target="_blank" aria-label="telegram">
-                        <i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm121.8 169.9l-40.7 191.8c-3 13.6-11.1 16.9-22.4 10.5l-62-45.7-29.9 28.8c-3.3 3.3-6.1 6.1-12.5 6.1l4.4-63.1 114.9-103.8c5-4.4-1.1-6.9-7.7-2.5l-142 89.4-61.2-19.1c-13.3-4.2-13.6-13.3 2.8-19.7l239.1-92.2c11.1-4 20.8 2.7 17.2 19.5z" className=""></path></svg></i>
+                        <a className="social social-linkedin" href="#" target="_blank" aria-label="telegram">
+                        <i><svg xmlns="http://www.w3.org/2000/svg" viewBox="-315 -315 1150 1150"><path fill="currentColor" d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm121.8 169.9l-40.7 191.8c-3 13.6-11.1 16.9-22.4 10.5l-62-45.7-29.9 28.8c-3.3 3.3-6.1 6.1-12.5 6.1l4.4-63.1 114.9-103.8c5-4.4-1.1-6.9-7.7-2.5l-142 89.4-61.2-19.1c-13.3-4.2-13.6-13.3 2.8-19.7l239.1-92.2c11.1-4 20.8 2.7 17.2 19.5z" className=""></path></svg></i>
                         </a>
                         <a href="#" className="social social-youtube" aria-label="youtube"><i className="ui-youtube"></i></a>
                         <a href="#" className="social social-instagram" aria-label="instagram"><i className="ui-instagram"></i></a>
